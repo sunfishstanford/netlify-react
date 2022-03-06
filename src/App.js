1c8bb99460b68a0f5f538a6bc818de5c3dd4bd63
Francis Ho <ho.francis@gmail.com>
2022-03-05 16:26:49 -0800

    1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This is a webapp on Netlify <br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
